62207cafa6642871cada96e1a0823e5b3e5e31d8
Mogeko <zhengjunyi@live.com>
2018-08-02 20:57:32 +0800

    Hexo 美化

--- source/js/src/motion.js
+++ source/js/src/motion.js
@@ -352,3 +352,10 @@ $(document).ready(function() {
   };
 
 });
+
+// 顶部阅读进度条
+$(document).ready(function () {
+  $(window).scroll(function(){
+    $(".top-scroll-bar").attr("style", "width: " + ($(this).scrollTop() / ($(document).height() - $(this).height()) * 100) + "%; display: block;");
+  });
+});
